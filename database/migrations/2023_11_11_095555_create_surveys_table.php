40443d27231c32bce2d690c761c9e459d55962b1
Hiroki-sato9 <resumi30minutes@gmail.com>
2023-11-11 10:59:54 +0900

    Survey, Questionモデルの作成

---
+++ database/migrations/2023_11_11_095555_create_surveys_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('description', 200);
+            $table->integer('answer_num')->default(0);
+            $table->integer('answer_limit')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
