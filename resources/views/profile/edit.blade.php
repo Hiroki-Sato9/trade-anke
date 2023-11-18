43c5aa4b40d3e10a8996b9cdbcb057b88efe269a
Hiroki-sato9 <resumi30minutes@gmail.com>
2023-11-18 15:57:15 +0900

    プロフィール画面にリンクを追加

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -4,6 +4,8 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
+    
+    <a href="/profile/detail">Detail</a>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
