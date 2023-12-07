a67351a33d04aee4d2eeed7c59ce2e7e5ac2decb
Hiroki-sato9 <resumi30minutes@gmail.com>
2023-12-07 21:22:20 +0900

    質問保存画面のJavaScriptでの処理

--- public/js/store_interviews.js
+++ public/js/store_interviews.js
@@ -1,1 +1,20 @@
 
+const posts = document.querySelectorAll(".post");
+
+const question_form = document.querySelector("#question");
+const answer_form = document.querySelector("#answer");
+
+
+posts.forEach((post) => {
+    post.addEventListener("click", () => {
+        let post_body = post.querySelector("div").textContent;
+        // postにrequest_userクラスを持つ要素があれば
+        if (post.classList.contains("request_user")){
+            console.log("request user");
+            question_form.value = post_body;
+        }else if(post.classList.contains("requested_user")){
+            answer_form.value = post_body;
+            console.log("requested user");
+        }
+    })
+});
