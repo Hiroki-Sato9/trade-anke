0d2be2c87c6efa6037cdfe388e21a81c5754c83c
Hiroki-sato9 <resumi30minutes@gmail.com>
2023-12-26 17:00:15 +0900

    aboutページでtailwindを適用

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="mx-auto">
+            <h1>タイトル</h1>
+            <p>概要文</p>
+            <button>ユーザー登録</button>
+            <h2>Trade-Ankeで できること</h2>
+        </div>
+    </body>
+</html>
