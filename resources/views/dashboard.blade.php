b9fa4ba90f1458c5d61a22fd88464fc96843d4dc
Hiroki-sato9 <resumi30minutes@gmail.com>
2023-12-28 23:14:01 +0900

    dashboardに説明文の追加

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,22 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
+            <h1 class="text-5xl py-5 mb-3">ようこそ</h1>
+            <p class="text-lg mb-12">このサービスの使い方を説明します</p>
+            <ul class="list-none">
+                <li class="mb-6">
+                    <h2 class="text-4xl border-t py-4 mb-4">手順１：配られたアンケートに答えてみよう</h2>
+                    <p><a href="#">ここ</a>で、あなたのプロフィールと合致したアンケートが配られます。</p>
+                </li>
+                <li class="mb-6">
+                    <h2 class="text-4xl border-t py-4 mb-4">手順２：アンケートを作ってみよう</h2>
+                    <p><a href="#">ここ</a>で、アンケートを作成することができます。</p>
+                </li>
+                <li class="mb-6">
+                    <h2 class="text-4xl border-t py-4 mb-4">手順３：アンケートを配ってみよう</h2>
+                    <p>作成したアンケートは、自分が持っているポイントと引き換えに誰かに届けることができます。</p>
+                </li>
+            </ul>
         </div>
     </div>
 </x-app-layout>
--- resources/views/static_pages/about.blade.php
+++ resources/views/static_pages/about.blade.php
@@ -17,7 +17,7 @@
         <div class="py-36">
         <div class="max-w-7xl mx-auto flex flex-col flex-wrap content-center">
             <h1 class="text-5xl mb-3">Trade-Ankeで、気軽にアンケート調査を実施しよう</h1>
-            <p class="text-lg mb-8">このサービスでは、自分が作成したアンケートを配布することができます。</p>
+            <p class="text-lg mb-12">このサービスでは、自分が作成したアンケートを配布することができます。</p>
             
             <div class="flex justify-around">
                 <div class="flex flex-col">
@@ -29,8 +29,20 @@
                     <button onclick="location.href='{{ route('surveys.index') }}'" class="w-3/5 bg-blue-500 hover:bg-blue-400 text-white rounded px-4 py-2 mb-20">アンケート一覧へ</button>
                 </div>
             </div>
+            <div class="items-center pb-6">
+                <span>すでにアカウントを持っている場合</span>
+                <a href=" {{ route('login') }}" clas="bg-blue">ログイン</a>
+            </div>
             <h2 class="text-4xl border-t py-4 mb-4">Trade-Ankeで できること</h2>
             <ul class="list-none">
+                <li class="mb-6 h-80 pt-10">
+                    <div class="content">
+                        <h3 class="text-3xl mb-3">アンケートを簡単に作成できる！</h3>
+                        <p class="text-lg">
+                            サイト内のフォームにて簡単に質問フォームを作成できます。
+                        </p>
+                    </div>
+                </li>
                 <li class="mb-6 h-80 pt-10">
                     <div class="content">
                         <h3 class="text-3xl mb-3">アンケートを答えるたびにポイントをゲット！</h3>
