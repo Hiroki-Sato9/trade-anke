53d6b0456ef384afde8dbb4b7475ad7bb085bc98
Hiroki-sato9 <resumi30minutes@gmail.com>
2024-01-31 20:47:58 +0900

    API通信成功

--- app/Http/Controllers/GoogleFormsController.php
+++ app/Http/Controllers/GoogleFormsController.php
@@ -39,24 +39,41 @@ class GoogleFormsController extends Controller
     
     public function test(Request $request)
     {
-        // dd(session('google_access_token'));
-        $form_service = new FormsAPIService('https://docs.google.com/forms/d/e/1FAIpQLSeSDM5wYGvsn7zAmH49F9ghUSVisqE19aBUdibbwyvpDXHAyQ/viewform?usp=sf_link',$request->url());
+        $redirect_uri = $request->url();
         
-        // dd($form_service);
-        if ($request->get('code')) {
-            $form_service->set_token($request->get('code'));
-            // $form_data = $form_service->service->forms->get($form_service->id);
-            // dd($form_data);
-             header('Location: ' . filter_var($request->url(), FILTER_SANITIZE_URL));
+        $client = new Google_Client();
+        $client->setAuthConfig(config_path() . '/google_client_secret.json');
+        $client->setRedirectUri($redirect_uri);
+        $client->addScope(Google_Service_Forms::FORMS_BODY);
+        $client->setAccessType('offline');
+        $service = new Google_Service_Forms($client);
+        
+        if (!empty($request->get('code'))) {
+            $token = $client->fetchAccessTokenWithAuthCode($request->get('code'), session('code_verifier'));
+            $client->setAccessToken($token);
+            header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
         }
         
-        if ($form_service->client->getAccessToken()) {
-            $data = $form_service->service->forms->get($form_service->id);
+        if (!empty(session('upload_token'))) {
+            $client->setAccessToken(session('upload_token'));
+            if ($client->isAccessTokenExpired()) {
+                session()->forget('upload_token');
+            }
+        } else {
+            session()->put('code_verifier', $client->getOAuth2Service()->generateCodeVerifier());
+            $auth_url = $client->createAuthUrl();
+        }
+        
+        if ($client->getAccessToken()) {
+            // dd($client->getAccessToken());
+            $data = $service->forms->get('1-fLk6OQWXuQswmxohkYs9U3W304SF81IDg4rOWMBWPk');
             dd($data);
         }
-        
+
         
         return view('static_pages.test')
-            ->with(['auth_url' => $form_service->auth_url]);
+            ->with(['auth_url' => isset($auth_url) ? $auth_url : null]);
     }
 }
+
+        // $form_service = new FormsAPIService('https://docs.google.com/forms/d/e/1FAIpQLSeSDM5wYGvsn7zAmH49F9ghUSVisqE19aBUdibbwyvpDXHAyQ/viewform?usp=sf_link',$request->url());
--- resources/views/static_pages/test.blade.php
+++ resources/views/static_pages/test.blade.php
@@ -1,4 +1,6 @@
 
 <x-app-layout>
+    @if ($auth_url)
     <a href="{{ $auth_url }}">Connect Me</a>
+    @endif
 </x-app-layout>
